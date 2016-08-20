477932ba4ff9af65a97e6e0e040d687dae73190f
Lonnie Jones <lonnie34j@gmail.com>
2016-08-20 02:30:50 -0400

    initial save files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,24 @@ $(".startButton").on("click", function(){
 	//reveals the quiz when the user clicks start and hides the start button after click
 	$(".quizContent").show();
 	$(".startButton").hide();
+
+	//sets the count for the timer
+	count = 200;
+	counter = setInterval(gameTimer, 200);
+
+	//runs the timer
+	function gameTimer(){
+
+		count -= 1;
+		if(count ==0){
+
+		$(".quizContent").hide();
+		$(".results").show();
+
+	}
+		$(".liveTime").html(count);
+	}
+
 });
 
 //done button finishs quiz and displays results

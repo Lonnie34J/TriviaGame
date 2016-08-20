8f35dc7143fe399ba7fe91cba05c150416e86efb
Lonnie Jones <lonnie34j@gmail.com>
2016-08-20 00:08:11 -0400

    initial save files

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+//hides the quiz content when the webpage loads
+$(".quizContent").hide();
+$(".results").hide();
+
+//Button that starts the quiz
+$(".startButton").on("click", function(){
+
+	//reveals the quiz when the user clicks start and hides the start button after click
+	$(".quizContent").show();
+	$(".startButton").hide();
+});
+
+//done button finishs quiz and displays results
+$(".doneButton").on("click",function(){
+
+	//$(".quizContent").hide();
+	$(".results").show();
+
+})
+
+
+});
